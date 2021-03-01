e355fe4f6b9241bc4e4791facf37234779474e0a
Nike-one <nikebro2000@gmail.com>
2021-03-01 18:50:55 +0530

    Third Commit

---
+++ maxOfThree.py
@@ -0,0 +1,12 @@
+def max(a,b):
+	if(a>b):
+		return a
+	else:
+		return b
+
+print("Enter three numbers: ")
+n1 =int(input())
+n2 = int(input())
+n3 = int(input())
+
+print("Max of three", max(n1,max(n2,n3)))
