4d9931f3bc9268b7a56216b9388180fe08a2bc79
Nike-one <nikebro2000@gmail.com>
2021-03-05 18:24:53 +0530

    fibonacci using recursion

---
+++ fibonacciSeries.py
@@ -0,0 +1,13 @@
+def fib(n):
+    if n <=1:
+        return n
+        
+    else:
+        return fib(n-1) + fib(n-2)
+        
+n1 = int(input("enter number: "))
+
+for i in range(0,n1):
+    print(fib(i))
+
+#output enter number: 5 --> 0 1 1 2 3
